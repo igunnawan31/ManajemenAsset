5cd382bd3877d31f075ec395d096d74a7b52d4e2
Muhamad Gunawan <igunnawan25@gmail.com>
2025-03-23 06:55:47 +0700

    adding create inbound page

--- backend/DTO/Ticket/CreateTicketDTO.cs
+++ backend/DTO/Ticket/CreateTicketDTO.cs
@@ -3,8 +3,6 @@ namespace qrmanagement.backend.DTO.Ticket{
     public class CreateTicketDTO{
         public required int branchOrigin {get; set;}
         public required int branchDestination {get; set;}
-        public DateOnly outboundDate {get; set;}
-        public DateOnly inboundDate {get; set;}
         public required DateOnly dateRequested {get; set;}
         public required string approvalStatus {get; set;}
         public required string moveStatus {get; set;}
