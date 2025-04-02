72d06a95e1d4acd7a1b22ef01ec72e6710fc2e1c
alcendol <gubranaldisar@gmail.com>
2025-04-02 20:58:05 +0700

    modify assetmove

--- backend/Migrations/20250402135734_modifyAssetMove.Designer.cs
+++ backend/Migrations/20250402135734_modifyAssetMove.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using qrmanagement.backend.Context;
 
@@ -11,9 +12,11 @@ using qrmanagement.backend.Context;
 namespace qrmanagement.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250402135734_modifyAssetMove")]
+    partial class modifyAssetMove
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Migrations/20250402135734_modifyAssetMove.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace qrmanagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class modifyAssetMove : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AssetMoves",
+                keyColumn: "id",
+                keyValue: 8,
+                column: "moveStatus",
+                value: "Pending");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AssetMoves",
+                keyColumn: "id",
+                keyValue: 8,
+                column: "moveStatus",
+                value: "Waiting");
+        }
+    }
+}
